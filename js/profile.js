f54995841040be188b7b764c95bd8f5834b41541
iooi-alice <kimhee21c@hanmail.net>
2024-08-28 12:51:07 +0900

    feat: profile 모달 토글 및 외부 클릭 시 닫히는 기능 구현

--- js/profile.js
+++ js/profile.js
@@ -1,8 +1,27 @@
-const profileButton = document.querySelector('.profile');
-const profileArrowIcon = profileButton.querySelector('.profile-info img');
+const profile = document.querySelector('.profile');
+const [profileButton, profileModal] = profile.children;
+const profileArrowIcon = profileButton.querySelector('.profile-button-info img');
+
+function closeProfileOnClickOutside(modal, containerElement) {
+  return function (event) {
+    if (!containerElement.contains(event.target)) {
+      modal.classList.remove('is-active');
+      profileButton.classList.remove('is-active');
+      profileArrowIcon.setAttribute('src', './assets/svg/ic-profile-arrow-down.svg');
+      profileArrowIcon.style.transform = 'rotate(0deg)';
+
+      window.removeEventListener('click', closeProfileOnClickOutside);
+    }
+  };
+}
 
 function toggleProfileModal() {
+  if (!profileModal.classList.contains('is-active')) {
+    window.addEventListener('click', closeProfileOnClickOutside(profileModal, profile));
+  }
+
   profileButton.classList.toggle('is-active');
+  profileModal.classList.toggle('is-active');
 
   if (profileButton.classList.contains('is-active')) {
     profileArrowIcon.setAttribute('src', './assets/svg/ic-profile-arrow-down-active.svg');
