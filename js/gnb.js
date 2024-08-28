e4912c204b317c2efac8b937074c1d616328789b
iooi-alice <kimhee21c@hanmail.net>
2024-08-28 12:50:31 +0900

    feat: gnb home 버튼 클릭 시 svg 이미지 변경되는 기능 구현

--- js/gnb.js
+++ js/gnb.js
@@ -1,17 +1,27 @@
-const gnbMenuButton = document.querySelectorAll('.gnb-menu-item a');
+const gnbMenuButtons = document.querySelectorAll('.gnb-menu-item a');
+const homeImgElement = document.querySelector('.gnb-menu-item.is-home img');
 
-let currentActive = document.querySelector('.gnb-menu-item.is-active');
+let currentActiveItem = document.querySelector('.gnb-menu-item.is-active');
 
-function menuActive() {
+function updateActiveMenu() {
   const liElement = this.parentNode;
 
-  if (currentActive !== liElement) {
+  if (currentActiveItem !== liElement) {
     liElement.classList.add('is-active');
-    currentActive.classList.remove('is-active');
-    currentActive = liElement;
+    currentActiveItem.classList.remove('is-active');
+    currentActiveItem = liElement;
+
+    updateHomeImgSrc(liElement);
   }
 }
 
-gnbMenuButton.forEach((menu) => {
-  menu.addEventListener('click', menuActive);
+function updateHomeImgSrc(activeItem) {
+  const isActiveHome = activeItem.classList.contains('is-home');
+  const newSrc = isActiveHome ? './assets/svg/ic-dashboard-active.svg' : './assets/svg/ic-dashboard-default.svg';
+
+  homeImgElement.setAttribute('src', newSrc);
+}
+
+gnbMenuButtons.forEach((button) => {
+  button.addEventListener('click', updateActiveMenu);
 });
