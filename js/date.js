9268a6d854398fe9283d9a04ae979f282ecd5600
iooi-alice <kimhee21c@hanmail.net>
2024-09-02 05:10:42 +0900

    refactor: date field 관련 기능 create 파일 내부 구현으로 변경되면서 필요 없는 파일 삭제

--- js/date.js
+++
@@ -1,4 +0,0 @@
-const dateInput = document.querySelector('.date-field input');
-
-dateInput.addEventListener('focus', () => dateInput.classList.add('is-active'));
-dateInput.addEventListener('blur', () => dateInput.classList.remove('is-active'));
