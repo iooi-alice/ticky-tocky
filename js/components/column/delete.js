1d1304771d2690f48a1438ab70ae2932c86be140
iooi-alice <kimhee21c@hanmail.net>
2024-09-03 04:22:19 +0900

    feat: column 삭제 기능 구현

--- js/components/column/delete.js
+++ js/components/column/delete.js
@@ -3,15 +3,25 @@ import { openModal, closeModal } from '../modal/modalOpenClose.js';
 const columnDeleteButtons = document.querySelectorAll('.task-options-wrapper .delete-button');
 const deleteModal = document.getElementById('detele-modal');
 const deleteModalCloseButton = deleteModal.querySelector('.cancel-button');
+const deleteModalSubmitButton = deleteModal.querySelector('.delete-button');
+
+let columnElement = null;
 
 function openColumnDeleteModal() {
   const optionsContainerElement = this.closest('.task-column-options');
   const [kebabButton, options] = optionsContainerElement.children;
+  columnElement = this.closest('.task-column-item');
 
   kebabButton.classList.remove('is-active');
   options.classList.remove('is-active');
 
   openModal(deleteModal);
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  columnElement.remove();
+  closeModal(deleteModal);
 }
 
 columnDeleteButtons.forEach((button) => {
@@ -20,3 +30,4 @@ columnDeleteButtons.forEach((button) => {
 
 deleteModalCloseButton.addEventListener('click', () => closeModal(deleteModal));
 deleteModal.addEventListener('close', () => closeModal(deleteModal));
+deleteModalSubmitButton.addEventListener('click', handleSubmit);
