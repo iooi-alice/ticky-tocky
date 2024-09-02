d0acade1a7182246ef75f762482f1f17f1218c94
iooi-alice <kimhee21c@hanmail.net>
2024-09-03 02:25:12 +0900

    refactor: PC, 모바일 gnb 메뉴 클릭 시 서로 연동되도록 개선

--- js/components/gnb/gnb.js
+++ js/components/gnb/gnb.js
@@ -1,16 +1,21 @@
-const gnbMenuButtons = document.querySelectorAll('.gnb-menu-item a');
-const homeImgElement = document.querySelector('.gnb-menu-item.is-home img');
+const homeImgElements = document.querySelectorAll('.gnb-menu-item.is-home img');
+
+const gnbMenuButtons = document.querySelectorAll('.gnb-menu-item');
+const gnbMenuImages = document.querySelectorAll('.gnb .gnb-menu-item img');
+
+const gnbMobileMenuButtons = document.querySelectorAll('.gnb-mobile .gnb-menu-item');
+const gnbMobileMenuImages = document.querySelectorAll('.gnb-mobile .gnb-menu-item img');
 
 let currentActiveItem = document.querySelector('.gnb-menu-item.is-active');
 
-function updateActiveMenu() {
-  const liElement = this.parentNode;
-
+function updateActiveMenu(liElement) {
   if (currentActiveItem !== liElement) {
-    liElement.classList.add('is-active');
     currentActiveItem.classList.remove('is-active');
+    liElement.classList.add('is-active');
     currentActiveItem = liElement;
 
+    syncMobileMenu(liElement);
+    syncPCMenu(liElement);
     updateHomeImgSrc(liElement);
   }
 }
@@ -19,9 +24,37 @@ function updateHomeImgSrc(activeItem) {
   const isActiveHome = activeItem.classList.contains('is-home');
   const newSrc = isActiveHome ? './assets/svg/ic-dashboard-active.svg' : './assets/svg/ic-dashboard-default.svg';
 
-  homeImgElement.setAttribute('src', newSrc);
+  homeImgElements.forEach((image) => {
+    image.setAttribute('src', newSrc);
+  });
 }
 
-gnbMenuButtons.forEach((button) => {
-  button.addEventListener('click', updateActiveMenu);
+function syncMobileMenu(activeItem) {
+  const activePCImage = activeItem.querySelector('img').getAttribute('alt');
+
+  gnbMobileMenuImages.forEach((gnbMobileImage) => {
+    const menuItem = gnbMobileImage.closest('.gnb-menu-item');
+    menuItem.classList.toggle('is-active', gnbMobileImage.getAttribute('alt') === activePCImage);
+  });
+}
+
+function syncPCMenu(activeItem) {
+  const activeMobileImage = activeItem.querySelector('img').getAttribute('alt');
+
+  gnbMenuImages.forEach((gnbImage) => {
+    const menuItem = gnbImage.closest('.gnb-menu-item');
+    menuItem.classList.toggle('is-active', gnbImage.getAttribute('alt') === activeMobileImage);
+  });
+}
+
+gnbMenuButtons.forEach((menu) => {
+  menu.addEventListener('click', function () {
+    updateActiveMenu(this);
+  });
+});
+
+gnbMobileMenuButtons.forEach((menu) => {
+  menu.addEventListener('click', function () {
+    updateActiveMenu(this);
+  });
 });
