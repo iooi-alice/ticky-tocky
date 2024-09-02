a6901d2203120060faa1d6c49b989349f033f1e1
iooi-alice <kimhee21c@hanmail.net>
2024-09-02 23:27:04 +0900

    refactor: 모바일인 경우에만 클릭 이벤트 발생하도록 변경, column 닫힌 경우 option 버튼 보이지 않도록 수정

--- js/components/column/toggleColumn.js
+++ js/components/column/toggleColumn.js
@@ -1,8 +1,14 @@
 const columnList = document.querySelectorAll('.task-column');
+const columnHeader = document.querySelectorAll('.task-column-header-content');
+const columnOptions = document.querySelectorAll('.task-column-options');
+
 const MOBILE_WIDTH = 767;
 
 function toggleColumn() {
-  this.classList.toggle('is-close');
+  const column = this.closest('.task-column');
+  const optionsOpenButton = column.querySelector('.task-column-options');
+  optionsOpenButton.classList.toggle('is-empty');
+  column.classList.toggle('is-close');
 }
 
 function handleResize() {
@@ -12,17 +18,24 @@ function handleResize() {
     columnList.forEach((column) => {
       column.classList.add('is-close');
     });
+
+    columnOptions.forEach((option) => {
+      option.classList.add('is-empty');
+    });
+
+    columnHeader.forEach((column) => {
+      column.addEventListener('click', toggleColumn);
+    });
   } else if (currentInnerWidth > MOBILE_WIDTH) {
     columnList.forEach((column) => {
       column.classList.remove('is-close');
+    });
+
+    columnOptions.forEach((option) => {
+      option.classList.remove('is-empty');
     });
   }
 }
 
 document.addEventListener('DOMContentLoaded', handleResize);
-
-columnList.forEach((column) => {
-  column.addEventListener('click', toggleColumn);
-});
-
 window.addEventListener('resize', handleResize);
